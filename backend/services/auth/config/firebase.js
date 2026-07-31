e375966aa8fdb8808939e4497d39803c3df6ced3
Ayush452-driod <mkccoc2@gmail.com>
2026-07-31 12:04:05 +0530

    firebase issue render fix

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,8 +1,12 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY
-);
+const firebaseKey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+
+if (!firebaseKey) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY is not defined");
+}
+
+const serviceAccount = JSON.parse(firebaseKey);
 
 if (!admin.apps.length) {
   admin.initializeApp({
